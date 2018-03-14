ad27ab5ee784ff3a074029e606a056faedb2a677
Juan Esquivias <juanesquiviasotazu@gmail.com>
2018-03-14 11:19:42 -0500

    add normalize.css v 5.0.0

--- wp-content/themes/thecakeshop/functions.php
+++ wp-content/themes/thecakeshop/functions.php
@@ -1,7 +1,15 @@
 <?php 
 
 function thecakeshop_styles() {
-	wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0' );
+
+	 //Register styles
+	wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '5.0.0');
+
+	wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0' );
+
+	 //Call styles
+	wp_enqueue_style('normalize');
+	wp_enqueue_style('style');
 }
 
 add_action('wp_enqueue_scripts', 'thecakeshop_styles');
