e835110c7852ec196c815654f6ae0b6f0194135e
Juan Esquivias <juanesquiviasotazu@gmail.com>
2018-03-14 10:58:08 -0500

    add css in theme

--- wp-content/themes/thecakeshop/index.php
+++ wp-content/themes/thecakeshop/index.php
@@ -1,1 +1,11 @@
-<h1>Hi world!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>The Cake Shop</title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<h1>Hi world!</h1>
+</body>
+</html>
